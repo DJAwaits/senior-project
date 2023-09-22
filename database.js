730bc26bfbaafa2959c0721eaeec6e8572b30cd3
rmrjSP <92806752+rmrjSP@users.noreply.github.com>
2023-09-22 15:20:37 -0700

    922

--- database.js
+++ database.js
@@ -7,5 +7,23 @@ const pool = mysql.createPool({
     database: 'senror_project'
 }).promise()
 
-const result = await pool.query("Select * from users")
-console.log(result)
+async function createUser(username, password, email) {
+    const result = await pool.query(`INSERT INTO users (username, password, email)
+    values (?, ?, ? )`, [username, password, email])
+    return result.insertId
+}
+
+async function getUsers() {
+    const [rows] = await pool.query("Select * from users")
+    return rows
+}
+
+async function getUser(id) {
+    const [rows] = await pool.query(`Select * from users Where userid = ?`, [id])
+    return rows[0]
+}
+
+
+
+const obj = await createUser('test', 'test', 'test@gmail.com')
+console.log(obj)
